3cda7c604baea18a188441ec17a776ef0a83dd48
anaismoreaugeo <anaismoreau.geo@gmail.com>
2021-02-26 11:31:34 +0100

    secteur activite files

---
+++ 2_Evolution_de_l_emploi/2_3_Secteur_activite/main.js
@@ -0,0 +1,30 @@
+Highcharts.chart('graphique', {
+    chart: {
+        type: 'spline'
+    },
+    title: {
+        text: 'Evolution de l’emploi salarié à Paris 2011-2020'
+    },
+
+    data: {
+        csvURL: 'https://raw.githubusercontent.com/Atelier-parisien-d-urbanisme/Observatoire-economie-paris/main/2_Evolution_de_l_emploi/2_3_Secteur_activite/secteur_activite.csv',
+        enablePolling: true
+    },
+
+    caption: {
+        text: 'Données CVS, en fin de trimestre, mises à jour le 11/02/2021. Sources: Insee, estimations d emploi d estimations trimestrielles Acoss-Urssaf, Dares, Insee.'
+    },
+
+    credits: {
+      enabled:false
+    },
+
+    yAxis: {
+     title: {
+        enabled: false,
+      }
+     },
+    legend: {
+     enabled: false
+    },
+});
