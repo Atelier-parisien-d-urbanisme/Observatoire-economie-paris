c581c7b492f9bc4ee3d3ca6ee0eb4e2bb3a847e7
anaismoreaugeo <anaismoreau.geo@gmail.com>
2021-03-08 09:45:21 +0100

    style start ups

--- 5_Secteurs_d_activites_les_plus_impactes/5_5_Les_start-ups/main.js
+++ 5_Secteurs_d_activites_les_plus_impactes/5_5_Les_start-ups/main.js
@@ -1,42 +1,27 @@
 Highcharts.setOptions({
-    colors: ['#38cae9']
+    colors: ['#9CE1F0','#38cae9','#128EA8']
 });
 
 
 Highcharts.chart('graphique', {
     chart: {
-        type: 'spline'
+        type: 'pie'
     },
     title: {
-        text: 'Evolution de l’emploi salarié à Paris 2011-2020'
+        text: 'Répartition des startups accompagnées par Paris&Co selon le risque encouru (octobre 2020)'
     },
 
     data: {
-        csvURL: 'https://raw.githubusercontent.com/Atelier-parisien-d-urbanisme/Observatoire-economie-paris/main/2_Evolution_de_l_emploi/2_1_Emploi_salarie/emploi_salarie.csv',
+        csvURL: 'https://raw.githubusercontent.com/Atelier-parisien-d-urbanisme/Observatoire-economie-paris/main/5_Secteurs_d_activites_les_plus_impactes/5_5_Les_start-ups/start-ups.csv',
         enablePolling: true
     },
 
     caption: {
-        text: 'Données CVS, en fin de trimestre, mises à jour le 11/02/2021. Sources: Insee, estimations d emploi d estimations trimestrielles Acoss-Urssaf, Dares, Insee.'
+        text: 'Source: Paris&Co'
     },
 
     credits: {
       enabled:false
-    },
-    plotOptions: {
-        series: {
-            marker: {
-                lineColor: '#00c3ff'
-            }
-        }
-    },
-    yAxis: {
-     title: {
-        enabled: false,
-      }
-     },
-    legend: {
-     enabled: false
     },
     exporting: {
             buttons: {
