fd9cf369589b485adb0a1197b4cb8dbaff7cd19b
anaismoreaugeo <anaismoreau.geo@gmail.com>
2021-03-15 18:13:40 +0100

    total

--- 1_Recul_de_l_activite/1_5_Auto_entrepreneurs/main.js
+++ 1_Recul_de_l_activite/1_5_Auto_entrepreneurs/main.js
@@ -14,13 +14,16 @@ Highcharts.chart('graphique', {
 
     data: {
         csvURL: 'https://raw.githubusercontent.com/Atelier-parisien-d-urbanisme/Observatoire-economie-paris/main/1_Recul_de_l_activite/1_5_Auto_entrepreneurs/auto_entrepreneurs.csv',
-        endColumn: 1,
+        endColumn: 2,
     },
 
     xAxis: {
         allowDecimals: false
     },
-
+    tooltip: {
+    pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y} €</b><br/>',
+     valueDecimals: 2
+    },
     series: [{
         name: 'Annual mean'
     }]
