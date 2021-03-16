246c4c3fcc8f88a824e62f0983fdeff63bef7599
anaismoreaugeo <anaismoreau.geo@gmail.com>
2021-03-16 09:56:18 +0100

    init

--- 1_Recul_de_l_activite/1_5_Auto_entrepreneurs/main.js
+++ 1_Recul_de_l_activite/1_5_Auto_entrepreneurs/main.js
@@ -3,6 +3,9 @@ Highcharts.setOptions({
 });
 
 Highcharts.chart('graphique', {
+  chart: {
+    type:'line',
+  },
     title: {
         text: 'Evolution du chiffre d’affaires déclaré des auto-entrepreneurs en Ile-de-France 2019-2020 (données provisoires)'
     },
@@ -47,11 +50,16 @@ series: [{
    }
 },{
     yAxis: 1,
+    type: 'areaspline',
+    dashStyle: 'Dot',
     data: {
     },
     tooltip: {pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y} %</b><br/>',
     valueDecimals: 2
-   }
+  },
+  color: '#D3BD00',
+  negativeColor: '#C2B778',
+  opacity: 0.3,
 }],
      exporting: {
          buttons: {
@@ -67,88 +75,15 @@ series: [{
              }
          }
      },
-
+     credits: {
+       enabled:false
+       },
 });
 
 // Highcharts.stockChart('graphique', {
-//     chart: {
-//         type: 'spline',
-//          height: 600,
-//     },
-//     title: {
-//         text: 'Evolution du chiffre d’affaires déclaré des auto-entrepreneurs en Ile-de-France 2019-2020 (données provisoires)'
-//     },
-//     scrollbar: {
-//       enabled: false
-//     },
-//     data: {
-//         csvURL: 'https://raw.githubusercontent.com/Atelier-parisien-d-urbanisme/Observatoire-economie-paris/main/1_Recul_de_l_activite/1_5_Auto_entrepreneurs/auto_entrepreneurs.csv',
-//         enablePolling: true,
-//         endColumn: 2,
-//     },
-//     caption: {
-//         text: 'Source : Urssaf, 2020'
-//     },
-//     navigator: {
-//       outlineWidth: 0,
-//       maskFill: 'rgba(0, 0, 0, 0.2)',
-//       handles: {
-//         symbols: ['o', 'o'],
-//         backgroundColor: 'grey',
-//         borderColor: 'grey',
-//       },
-//       xAxis: {
-//         gridLineWidth: 0,
-//         labels: {
-//           enabled: false
-//         }
-//       },
+//
 //       series: {
-//         // type: 'areaspline',
-//         color: '#bbb'
+//         //
 //       }
 //     },
-//     plotOptions: {
-//         series: {
-//           showInNavigator: true,
-//           marker: {
-//             enabled: false,
-//           },
-//         }
-//     },
-//     rangeSelector:{
-//       enabled:false
-//     },
-//     credits: {
-//       enabled:false
-//     },
-//     xAxis: {
-//           gapGridLineWidth: 0
-//     },
-//     legend: {
-//       enabled:true
-//     },
-//     yAxis: {
-//      title: {
-//         enabled: false,
-//       }
-//      },
-//      tooltip: {
-//          // pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y} %</b><br/>',
-//          valueDecimals: 2
-//      },
-//      exporting: {
-//          buttons: {
-//              contextButton: {
-//                  menuItems: [
-//                      'printChart',
-//                      'separator',
-//                      'downloadPNG',
-//                      'downloadJPEG',
-//                      'downloadPDF',
-//                      'downloadSVG'
-//                  ]
-//              }
-//          }
-//      },
 // });
