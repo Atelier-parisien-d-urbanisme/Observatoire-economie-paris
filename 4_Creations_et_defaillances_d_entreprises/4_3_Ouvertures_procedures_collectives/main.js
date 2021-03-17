12c254db121be95420bbb03149a622f5324c9d2c
anaismoreaugeo <anaismoreau.geo@gmail.com>
2021-03-17 11:02:47 +0100

    init ouvertures procedures collectives

--- 4_Creations_et_defaillances_d_entreprises/4_3_Ouvertures_procedures_collectives/main.js
+++ 4_Creations_et_defaillances_d_entreprises/4_3_Ouvertures_procedures_collectives/main.js
@@ -13,28 +13,19 @@ Highcharts.chart('graphique', {
         text: 'Source : Greffe du tribunal de commerce'
     },
     data: {
-        csvURL: 'https://raw.githubusercontent.com/Atelier-parisien-d-urbanisme/Observatoire-economie-paris/main/1_Recul_de_l_activite/1_5_Auto_entrepreneurs/auto_entrepreneurs.csv',
+        csvURL: 'https://raw.githubusercontent.com/Atelier-parisien-d-urbanisme/Observatoire-economie-paris/main/4_Creations_et_defaillances_d_entreprises/4_3_Ouvertures_procedures_collectives/ouverture_procedures_collectives.csv',
         startColumn:0,
         endColumn:3,
     },
 
-    yAxis: [{ //--- Primary yAxis
-    className: 'highcharts-color-0',
+    yAxis: [{
      title: {
-        text: '€'
+        text: '%'
     }
-}, { //--- Secondary yAxis
-    className: 'highcharts-color-1',
-    title: {
-        text: '%',
-        style: {
-            color: '#D3BD00'
-        }
-    },
-    opposite: true
 }]
 ,
 series: [{
+    type:'line',
     yAxis: 0,
     data: {
     },
@@ -42,24 +33,13 @@ series: [{
     valueDecimals: 2
    }
 },{
+  type:'column',
     yAxis: 0,
     data: {
     },
     tooltip: {pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y} €</b><br/>',
     valueDecimals: 2
    }
-},{
-    yAxis: 1,
-    type: 'areaspline',
-    dashStyle: 'Dot',
-    data: {
-    },
-    tooltip: {pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y} %</b><br/>',
-    valueDecimals: 2
-  },
-  color: '#D3BD00',
-  negativeColor: '#C2B778',
-  opacity: 0.3,
 }],
      exporting: {
          buttons: {
