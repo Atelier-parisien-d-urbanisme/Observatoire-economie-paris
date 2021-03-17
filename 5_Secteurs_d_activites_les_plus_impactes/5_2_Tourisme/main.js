c41e81701c8058bba266c90740535dcc17c2002b
anaismoreaugeo <anaismoreau.geo@gmail.com>
2021-03-17 11:59:56 +0100

    init

--- 5_Secteurs_d_activites_les_plus_impactes/5_2_Tourisme/main.js
+++ 5_Secteurs_d_activites_les_plus_impactes/5_2_Tourisme/main.js
@@ -4,16 +4,15 @@ Highcharts.setOptions({
 
 Highcharts.chart('graphique', {
   chart: {
-    type:'line',
   },
     title: {
-        text: 'Evolution du nombre d’ouvertures de procédures collectives à Paris 2019-2020'
+        text: 'Evolution du taux d’occupation hôtelière à Paris 2019 - 2020'
     },
     caption: {
-        text: 'Source : Greffe du tribunal de commerce'
+        text: 'Source : OTCP'
     },
     data: {
-        csvURL: 'https://raw.githubusercontent.com/Atelier-parisien-d-urbanisme/Observatoire-economie-paris/main/4_Creations_et_defaillances_d_entreprises/4_3_Ouvertures_procedures_collectives/ouverture_procedures_collectives.csv',
+        csvURL: 'https://raw.githubusercontent.com/Atelier-parisien-d-urbanisme/Observatoire-economie-paris/main/5_Secteurs_d_activites_les_plus_impactes/5_2_Tourisme/tourisme.csv',
         startColumn:0,
         endColumn:3,
     },
@@ -29,16 +28,16 @@ series: [{
     yAxis: 0,
     data: {
     },
-    tooltip: {pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y} €</b><br/>',
-    valueDecimals: 2
+    tooltip: {pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y} %</b><br/>',
+    valueDecimals: 0
    }
 },{
   type:'column',
     yAxis: 0,
     data: {
     },
-    tooltip: {pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y} €</b><br/>',
-    valueDecimals: 2
+    tooltip: {pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y} %</b><br/>',
+    valueDecimals: 0
    }
 }],
      exporting: {
