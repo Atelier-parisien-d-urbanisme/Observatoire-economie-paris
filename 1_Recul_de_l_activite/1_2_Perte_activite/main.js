d90a4d6016bb6ced253af90a4efe450e99765e93
anaismoreaugeo <anaismoreau.geo@gmail.com>
2021-03-18 15:13:51 +0100

    test

--- 1_Recul_de_l_activite/1_2_Perte_activite/main.js
+++ 1_Recul_de_l_activite/1_2_Perte_activite/main.js
@@ -1,81 +1,94 @@
-Highcharts.setOptions({
-    colors: ['#38cae9', '#2A8BA0','#D3BD00','#D39500']
-});
+// Prepare demo data
+// Data is joined to map using value of 'hc-key' property by default.
+// See API docs for 'joinBy' for more info on linking data and map.
+var data = [
+    ['mx-3622', 0],
+    ['mx-bc', 1],
+    ['mx-bs', 2],
+    ['mx-so', 3],
+    ['mx-cl', 4],
+    ['mx-na', 5],
+    ['mx-cm', 6],
+    ['mx-qr', 7],
+    ['mx-mx', 8],
+    ['mx-mo', 9],
+    ['mx-df', 10],
+    ['mx-qt', 11],
+    ['mx-tb', 12],
+    ['mx-cs', 13],
+    ['mx-nl', 14],
+    ['mx-si', 15],
+    ['mx-ch', 16],
+    ['mx-ve', 17],
+    ['mx-za', 18],
+    ['mx-ag', 19],
+    ['mx-ja', 20],
+    ['mx-mi', 21],
+    ['mx-oa', 22],
+    ['mx-pu', 23],
+    ['mx-gr', 24],
+    ['mx-tl', 25],
+    ['mx-tm', 26],
+    ['mx-co', 27],
+    ['mx-yu', 28],
+    ['mx-dg', 29],
+    ['mx-gj', 30],
+    ['mx-sl', 31],
+    ['mx-hg', 32]
+];
 
-Highcharts.chart('graphique', {
-  chart: {
-    type:'line',
-  },
-    title: {
-        text: 'Evolution du chiffre d’affaires déclaré des auto-entrepreneurs en Ile-de-France 2019-2020 (données provisoires)'
-    },
-    caption: {
-        text: 'Source : Urssaf, 2020'
-    },
-    data: {
-        csvURL: 'https://raw.githubusercontent.com/Atelier-parisien-d-urbanisme/Observatoire-economie-paris/main/1_Recul_de_l_activite/1_5_Auto_entrepreneurs/auto_entrepreneurs.csv',
-        startColumn:0,
-        endColumn:3,
+// Create the chart
+var chart = Highcharts.mapChart('container', {
+    chart: {
+        map: 'countries/mx/mx-all'
     },
 
-    yAxis: [{ //--- Primary yAxis
-    className: 'highcharts-color-0',
-     title: {
-        text: '€'
-    }
-}, { //--- Secondary yAxis
-    className: 'highcharts-color-1',
     title: {
-        text: '%',
-        style: {
-            color: '#D3BD00'
+        text: 'Highmaps basic demo'
+    },
+
+    subtitle: {
+        text: 'Source map: <a href="http://code.highcharts.com/mapdata/countries/mx/mx-all.js">Mexico</a>'
+    },
+
+    mapNavigation: {
+        enabled: true,
+        buttonOptions: {
+            verticalAlign: 'bottom'
         }
     },
-    opposite: true
-}]
-,
-series: [{
-    yAxis: 0,
-    data: {
+
+    colorAxis: {
+        min: 0
     },
-    tooltip: {pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y} €</b><br/>',
-    valueDecimals: 2
-   }
-},{
-    yAxis: 0,
-    data: {
-    },
-    tooltip: {pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y} €</b><br/>',
-    valueDecimals: 2
-   }
-},{
-    yAxis: 1,
-    type: 'areaspline',
-    dashStyle: 'Dot',
-    data: {
-    },
-    tooltip: {pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y} %</b><br/>',
-    valueDecimals: 2
-  },
-  color: '#D3BD00',
-  negativeColor: '#C2B778',
-  opacity: 0.3,
-}],
-     exporting: {
-         buttons: {
-             contextButton: {
-                 menuItems: [
-                     'printChart',
-                     'separator',
-                     'downloadPNG',
-                     'downloadJPEG',
-                     'downloadPDF',
-                     'downloadSVG'
-                 ]
-             }
-         }
-     },
-     credits: {
-       enabled:false
-       },
+
+    series: [{
+        data: data,
+        name: 'Random data',
+        states: {
+            hover: {
+                color: '#BADA55'
+            }
+        },
+        dataLabels: {
+            enabled: true,
+            formatter: function() {
+                if (this.point['hc-key'] === 'mx-df') {
+                    return 'CDMX';
+                }
+
+                return this.point.name;
+            }
+        }
+    }],
+
+    tooltip: {
+        pointFormatter: function() {
+            if (this['hc-key'] === 'mx-df') {
+                return 'CDMX: ' + this.value;
+            }
+
+            return this.name + ' ' + this.value;
+        }
+    }
 });
