fd6f8f2a24127943325f18e2d1210e48cd7832fb
anaismoreaugeo <anaismoreau.geo@gmail.com>
2021-04-06 18:09:01 +0200

    init

--- 3_Chomage_Rsa/3_4_Demandeur_emploi_arrondissement/main.js
+++ 3_Chomage_Rsa/3_4_Demandeur_emploi_arrondissement/main.js
@@ -85,8 +85,8 @@ Highcharts.getJSON('https://atelier-parisien-d-urbanisme.github.io/Observatoire-
         colorAxis: {
             tickPixelInterval: 40,
             reversed: false,
-            minColor: '#ECCCD2',
-           maxColor: '#990041',
+            minColor: '#C2CEEE',
+           maxColor: '#2953CB',
         //    stops: [
         //     [0, '#3060cf'],
         //     [0.8, '#fffbbc'],
