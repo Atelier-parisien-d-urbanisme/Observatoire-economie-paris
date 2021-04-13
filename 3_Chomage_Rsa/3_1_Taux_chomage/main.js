2907b23aa00534c0cbb05906fc2ba1b038c1a3f1
anaismoreaugeo <anaismoreau.geo@gmail.com>
2021-04-13 17:19:05 +0200

    init

--- 3_Chomage_Rsa/3_1_Taux_chomage/main.js
+++ 3_Chomage_Rsa/3_1_Taux_chomage/main.js
@@ -1,5 +1,5 @@
 Highcharts.setOptions({
-    colors: ['#38cae9', '#2A8BA0','#D3BD00','#D39500']
+    colors: ['#38cae9', '#2A8BA0','#3333cc']
 });
 
 Highcharts.chart('graphique', {
@@ -8,18 +8,13 @@ Highcharts.chart('graphique', {
         height:500
     },
     title: {
-        text: 'Evolution du taux de chômage (au sens du BIT) 2011-2020		'
+        text: null
     },
 
     data: {
         csvURL: 'https://raw.githubusercontent.com/Atelier-parisien-d-urbanisme/Observatoire-economie-paris/main/3_Chomage_Rsa/3_1_Taux_chomage/taux_chomage.csv',
         enablePolling: true
     },
-
-    caption: {
-        text: 'Notes : données trimestrielles CVS. Les données du dernier trimestre affiché sont provisoires. Source : Insee, taux de chômage au sens du BIT et taux de chômage. '
-    },
-
     credits: {
       enabled:false
     },
