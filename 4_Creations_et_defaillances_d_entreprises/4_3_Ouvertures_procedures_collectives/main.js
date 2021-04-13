c7cbaff837140f3d8951b0d2ba1628a917559a43
anaismoreaugeo <anaismoreau.geo@gmail.com>
2021-04-14 00:48:34 +0200

    init

--- 4_Creations_et_defaillances_d_entreprises/4_3_Ouvertures_procedures_collectives/main.js
+++ 4_Creations_et_defaillances_d_entreprises/4_3_Ouvertures_procedures_collectives/main.js
@@ -1,5 +1,5 @@
 Highcharts.setOptions({
-    colors: ['#38cae9', '#2A8BA0','#D3BD00','#D39500']
+    colors: ['#f0836d','#FADFD9']
 });
 
 Highcharts.chart('graphique', {
@@ -7,38 +7,58 @@ Highcharts.chart('graphique', {
     type:'line',
   },
     title: {
-        text: 'Evolution du nombre d’ouvertures de procédures collectives à Paris 2019-2020'
-    },
-    caption: {
-        text: 'Source : Greffe du tribunal de commerce'
+        text: null
     },
     data: {
         csvURL: 'https://raw.githubusercontent.com/Atelier-parisien-d-urbanisme/Observatoire-economie-paris/main/4_Creations_et_defaillances_d_entreprises/4_3_Ouvertures_procedures_collectives/ouverture_procedures_collectives.csv',
         startColumn:0,
         endColumn:3,
     },
+    plotOptions: {
+        series: {
+          lineWidth: 1,
+            marker: {
+              enabledThreshold: 2,
+              radius: 3,
+              fillColor: '#FFFFFF',
+              lineWidth: 2,
+              lineColor: null // inherit from series,
+            }
+        }
+    },
 
     yAxis: [{
+      gridLineColor: '#efefef',
+      gridLineDashStyle: 'dash',
+      labels: {
+        format:'{value}',
+        align: 'left',
+              x: 0,
+              y: -2,
+              style: {
+                  color: '#CFCFCF'
+              }
+          },
      title: {
-        text: '%'
-    }
-}]
+        enabled: false,
+      }
+      }]
 ,
 series: [{
     type:'line',
     yAxis: 0,
     data: {
     },
-    tooltip: {pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y} €</b><br/>',
-    valueDecimals: 2
+    tooltip: {pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b><br/>',
+    valueDecimals: 0
    }
 },{
   type:'column',
     yAxis: 0,
     data: {
     },
-    tooltip: {pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y} €</b><br/>',
-    valueDecimals: 2
+    tooltip: {pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b><br/>',
+    valueDecimals: 0
    }
 }],
      exporting: {
