fa971429cdc6425b418e06be374ca53e1813b795
anaismoreaugeo <anaismoreau.geo@gmail.com>
2021-04-14 03:46:18 +0200

    init

--- 6_Aides_et_mesures_de_soutien/6_3_Report_echeances_fiscales/main.js
+++ 6_Aides_et_mesures_de_soutien/6_3_Report_echeances_fiscales/main.js
@@ -1,5 +1,5 @@
 Highcharts.setOptions({
-    colors: ['#38cae9', '#2A8BA0','#D3BD00','#D39500']
+    colors: ['#b0a8d3']
 });
 
 Highcharts.chart('graphique', {
@@ -7,10 +7,7 @@ Highcharts.chart('graphique', {
       inverted: true
     },
     title: {
-        text: "Répartition du montant total des reports d'échéances fiscales accordés par secteur d’activité (en M€) à Paris"
-    },
-    caption: {
-        text: 'Source : https://www.economie.gouv.fr/ au 21 janvier 2021'
+        text: null
     },
     data: {
         csvURL: 'https://raw.githubusercontent.com/Atelier-parisien-d-urbanisme/Observatoire-economie-paris/main/6_Aides_et_mesures_de_soutien/6_3_Report_echeances_fiscales/report_echeances_fiscales.csv',
@@ -22,12 +19,14 @@ Highcharts.chart('graphique', {
 
   },
     yAxis: [{
+      gridLineColor: '#efefef',
+      gridLineDashStyle: 'dash',
       reversed: false,
      title: {
         enabled:false,
     },
     labels: {
-    format: '{value}M€',
+    format: '{value}',
   },
 }]
 ,
@@ -36,16 +35,17 @@ series: [{
     yAxis: 0,
     data: {
     },
-    tooltip: {pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y} M€</b><br/>',
+    tooltip: {pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y} </b><br/>',
     valueDecimals: 0
   },
   dataLabels: {
                enabled: true,
-               format: '{y}M€',
+               format: '{y}',
                style: {
                 fontSize: '10px',
                 fontFamily: 'Roboto',
-                color:'#38cae9',
+                textOutline: 0,
+                color:'#b0a8d3',
             }
            },
 }],
@@ -65,5 +65,8 @@ series: [{
      },
      credits: {
        enabled:false
+       },
+       legend: {
+         enabled:false,
        },
 });
