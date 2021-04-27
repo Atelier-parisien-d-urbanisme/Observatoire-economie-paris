02b468a33d93deff941536c6261ef62322611d9f
anaismoreaugeo <anaismoreau.geo@gmail.com>
2021-04-27 17:12:20 +0200

    init

--- 6_Aides_et_mesures_de_soutien/6_2_Pret_garanti_Etat/main.js
+++ 6_Aides_et_mesures_de_soutien/6_2_Pret_garanti_Etat/main.js
@@ -58,6 +58,32 @@ series: [{
            },
 }],
      exporting: {
+       chartOptions:{
+         // add logo, titles, and sources updated in in the html page
+         title: {
+           text: "."
+         },
+         caption: {
+           text: "Source : economie.gouv.fr. Le Prêt Garanti par l’État (PGE) est un prêt bancaire de trésorerie d’un an concernant les entreprises et les professionnels de toute taille, quelles que soient leurs formes juridiques et leurs activités. Le montant maximum cumulé d’emprunt pouvant bénéficier de cette garantie de l’État est de 25 % du chiffre d’affaires annuel. Pour les créateurs d’entreprise, les start-up et entreprises innovantes, avec souvent peu de chiffre d’affaires, ce montant peut cependant être porté à deux ans de masse salariale. https://www.apur.org/fr/geo-data/observatoire-economie-parisienne-donnees-conjoncturelles"
+         },
+           chart:{
+            events:{
+                load:function(){
+                    this.renderer.image('https://atelier-parisien-d-urbanisme.github.io/Observatoire-economie-paris/logo_apur.png', 0, 0, 50, 30).add();
+
+                    title = document.getElementById('title1').innerHTML;
+                    this.renderer.text(title, 70, 20)
+                     .css({
+                       color: 'black',
+                       fontFamily: 'Roboto',
+                       fontSize: 14,
+                       fontWeight: 'bold',
+                       width: 400
+                     }).add().toFront();
+                }
+            }
+        }
+      },
          buttons: {
              contextButton: {
                  menuItems: [
