0b44de64d5f1e5d4b5b420be4af47eaf7d580485
anaismoreaugeo <anaismoreau.geo@gmail.com>
2021-04-27 16:39:43 +0200

    init

--- 2_Evolution_de_l_emploi/2_2_Emploi_interimaire/main.js
+++ 2_Evolution_de_l_emploi/2_2_Emploi_interimaire/main.js
@@ -62,6 +62,32 @@ Highcharts.chart('graphique', {
     },
 
     exporting: {
+      chartOptions:{
+        title: {
+          text: "."
+        },
+        caption: {
+          text: "Sources: Insee, estimations d emploi d estimations trimestrielles Acoss-Urssaf, Dares. Données CVS, en fin de trimestre. Champ : emploi salarié total. https://www.apur.org/fr/geo-data/observatoire-economie-parisienne-donnees-conjoncturelles"
+        },
+          chart:{
+           events:{
+               load:function(){
+                  // add logo, titles, and sources update in in the html page
+                   this.renderer.image('https://atelier-parisien-d-urbanisme.github.io/Observatoire-economie-paris/logo_apur.png', 0, 0, 50, 30).add();
+
+                   title = document.getElementById('title1').innerHTML;
+                   this.renderer.text(title, 70, 20)
+                    .css({
+                      color: 'black',
+                      fontFamily: 'Roboto',
+                      fontSize: 14,
+                      fontWeight: 'bold',
+                      width: 400
+                    }).add().toFront();
+               }
+           }
+       }
+     },
         buttons: {
             contextButton: {
                 menuItems: [
