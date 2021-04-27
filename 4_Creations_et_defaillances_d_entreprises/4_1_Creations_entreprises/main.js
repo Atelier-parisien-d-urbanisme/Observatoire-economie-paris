080f1a63afb120c0984a1f91d7d7e134b50dd02c
anaismoreaugeo <anaismoreau.geo@gmail.com>
2021-04-27 16:58:37 +0200

    init

--- 4_Creations_et_defaillances_d_entreprises/4_1_Creations_entreprises/main.js
+++ 4_Creations_et_defaillances_d_entreprises/4_1_Creations_entreprises/main.js
@@ -119,6 +119,32 @@ series: [
     //   }
     //  },
     exporting: {
+      chartOptions:{
+        // add logo, titles, and sources updated in in the html page
+        title: {
+          text: "."
+        },
+        caption: {
+          text: "Source : Insee, Répertoire des entreprises et des établissements. Champ : activités marchandes hors agriculture. Données brutes. https://www.apur.org/fr/geo-data/observatoire-economie-parisienne-donnees-conjoncturelles"
+        },
+          chart:{
+           events:{
+               load:function(){
+                   this.renderer.image('https://atelier-parisien-d-urbanisme.github.io/Observatoire-economie-paris/logo_apur.png', 0, 0, 50, 30).add();
+
+                   title = document.getElementById('title1').innerHTML;
+                   this.renderer.text(title, 70, 20)
+                    .css({
+                      color: 'black',
+                      fontFamily: 'Roboto',
+                      fontSize: 14,
+                      fontWeight: 'bold',
+                      width: 400
+                    }).add().toFront();
+               }
+           }
+       }
+     },
             buttons: {
                 contextButton: {
                     menuItems: [
