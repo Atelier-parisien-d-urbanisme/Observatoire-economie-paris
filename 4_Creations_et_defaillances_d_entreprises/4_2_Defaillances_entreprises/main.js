102d28a171376d448f75687844b980103a4cce41
anaismoreaugeo <anaismoreau.geo@gmail.com>
2021-04-27 16:59:29 +0200

    init

--- 4_Creations_et_defaillances_d_entreprises/4_2_Defaillances_entreprises/main.js
+++ 4_Creations_et_defaillances_d_entreprises/4_2_Defaillances_entreprises/main.js
@@ -60,6 +60,33 @@ Highcharts.chart('graphique', {
      enabled: false
     },
     exporting: {
+      chartOptions:{
+        // add logo, titles, and sources updated in in the html page
+        title: {
+          text: "."
+        },
+        caption: {
+          text: "Source : Insee, Répertoire des entreprises et des établissements.Champ : par date de jugement - Données brutes. https://www.apur.org/fr/geo-data/observatoire-economie-parisienne-donnees-conjoncturelles"
+        },
+          chart:{
+           events:{
+               load:function(){
+                   this.renderer.image('https://atelier-parisien-d-urbanisme.github.io/Observatoire-economie-paris/logo_apur.png', 0, 0, 50, 30).add();
+
+                   title = document.getElementById('title1').innerHTML;
+                   this.renderer.text(title, 70, 20)
+                    .css({
+                      color: 'black',
+                      fontFamily: 'Roboto',
+                      fontSize: 14,
+                      fontWeight: 'bold',
+                      width: 400
+                    }).add().toFront();
+               }
+           }
+       }
+     }
+,
             buttons: {
                 contextButton: {
                     menuItems: [
