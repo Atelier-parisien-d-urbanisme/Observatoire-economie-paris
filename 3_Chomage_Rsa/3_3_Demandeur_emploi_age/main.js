b9384aa8d5f907a7197026d05175541c79efd128
anaismoreaugeo <anaismoreau.geo@gmail.com>
2021-05-11 18:49:07 +0200

    init

--- 3_Chomage_Rsa/3_3_Demandeur_emploi_age/main.js
+++ 3_Chomage_Rsa/3_3_Demandeur_emploi_age/main.js
@@ -88,8 +88,38 @@ Highcharts.stockChart('graphique', {
             lineWidth: 2,
             lineColor: null // inherit from series,
           },
-        }
-    },
+          dataLabels: {
+             enabled: true,
+             align: 'left',
+             crop: false,
+             style : {
+               fontFamily: 'Roboto'
+             },
+             formatter: function() {
+                 if (this.point.x == this.series.data.length - 1) {
+                   return Highcharts.numberFormat(this.y,0);
+                 } else {
+                     return null;
+                 }
+             },
+         }
+      }
+  },
+  responsive: {
+      rules: [{
+          condition: {
+              maxWidth: 560
+          },
+          // Labels space on mobile
+          chartOptions: {
+              xAxis: {
+                  labels: {
+                      step: 4,
+                  }
+              },
+          }
+      }]
+  },
     rangeSelector:{
       enabled:false
     },
