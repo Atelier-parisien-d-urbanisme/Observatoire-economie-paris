3395c03278c67503e31585d9f661bdae3fad6235
anaismoreaugeo <anaismoreau.geo@gmail.com>
2021-05-11 18:43:42 +0200

    init

--- 3_Chomage_Rsa/3_1_Taux_chomage/main.js
+++ 3_Chomage_Rsa/3_1_Taux_chomage/main.js
@@ -89,8 +89,39 @@ Highcharts.chart('graphique', {
             lineWidth: 2,
             lineColor: null // inherit from series,
           },
+          dataLabels: {
+             enabled: true,
+             align: 'left',
+             crop: false,
+             style : {
+               fontFamily: 'Roboto'
+             },
+             formatter: function() {
+                 if (this.point.x == this.series.data.length - 1) {
+                   return Highcharts.numberFormat(this.y,0);
+                 } else {
+                     return null;
+                 }
+             },
+         }
         }
     },
+    responsive: {
+        rules: [{
+            condition: {
+                maxWidth: 560
+            },
+            // Labels space on mobile
+            chartOptions: {
+                xAxis: {
+                    labels: {
+                        step: 4,
+                    }
+                },
+            }
+        }]
+    },
+
     exporting: {
       chartOptions:{
         title: {
