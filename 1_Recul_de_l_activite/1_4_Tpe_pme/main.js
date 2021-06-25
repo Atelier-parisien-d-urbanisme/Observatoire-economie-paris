84e5c1ee0be3e94ea761e88eeaab7c3f163b85cf
anaismoreaugeo <66426603+anaismoreaugeo@users.noreply.github.com>
2021-06-25 12:00:50 +0200

    carte tpe-pme

--- 1_Recul_de_l_activite/1_4_Tpe_pme/main.js
+++ 1_Recul_de_l_activite/1_4_Tpe_pme/main.js
@@ -40,12 +40,12 @@ Highcharts.getJSON('https://atelier-parisien-d-urbanisme.github.io/Observatoire-
         },
         colorAxis: {
             tickPixelInterval: 100,
-            reversed: true,
+            // reversed: true,
             // Manually change colored steps of the map
             stops: [
-              [0, '#990041'],
+              [0, '#F1DCE2'],
               [0.65, '#C75E8B'],
-              [0.85, '#F1DCE2']
+              [0.9, '#990041' ]
             ]
         },
         series: [{
