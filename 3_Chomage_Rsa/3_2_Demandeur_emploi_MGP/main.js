89e7e6529255ad3aabd18491994fd69e573e64bd
Anais Moreau <anaismoreau.geo@gmail.com>
2022-05-23 15:43:33 +0200

    init

--- 3_Chomage_Rsa/3_2_Demandeur_emploi_MGP/main.js
+++ 3_Chomage_Rsa/3_2_Demandeur_emploi_MGP/main.js
@@ -1,5 +1,5 @@
 Highcharts.setOptions({
-  colors: ['#38cae9','#3333cc'],
+  colors: ['#9494FA','#3333cc','#F1BB63','#CE8C34',],
     style: {
      fontFamily: 'Roboto'
    },
@@ -58,6 +58,13 @@ Highcharts.chart('graphique', {
       }
     }
   },
+  series: [{
+    },{dashStyle: 'dash',
+  },{visible:false,
+    },{dashStyle: 'dash',visible:false,
+  }
+    ],
+
     yAxis: {
       gridLineColor: '#efefef',
       gridLineDashStyle: 'dash',
