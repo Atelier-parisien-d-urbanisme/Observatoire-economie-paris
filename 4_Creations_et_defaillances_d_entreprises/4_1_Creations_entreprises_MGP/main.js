c0fcc78d7305e22550694da5c29c81c4c66f3db7
anaismoreaugeo <anaismoreau.geo@gmail.com>
2022-10-03 19:17:09 +0200

    init

--- 4_Creations_et_defaillances_d_entreprises/4_1_Creations_entreprises_MGP/main.js
+++ 4_Creations_et_defaillances_d_entreprises/4_1_Creations_entreprises_MGP/main.js
@@ -168,162 +168,162 @@ responsive: {
 
 
 
-Highcharts.chart('graphique2', {
-  chart: {
-      type: 'spline',
-      height:400,
-      style: {
-       fontFamily: 'Roboto'
-     }
-  },
-  title: {
-      text: null
-  },
+// Highcharts.chart('graphique2', {
+//   chart: {
+//       type: 'spline',
+//       height:400,
+//       style: {
+//        fontFamily: 'Roboto'
+//      }
+//   },
+//   title: {
+//       text: null
+//   },
 
-  data: {
-      csvURL: 'https://raw.githubusercontent.com/Atelier-parisien-d-urbanisme/Observatoire-economie-paris/main/4_Creations_et_defaillances_d_entreprises/4_1_Creations_entreprises_MGP/part_autoEnrepreneur.csv',
-      enablePolling: true
-  },
+//   data: {
+//       csvURL: 'https://raw.githubusercontent.com/Atelier-parisien-d-urbanisme/Observatoire-economie-paris/main/4_Creations_et_defaillances_d_entreprises/4_1_Creations_entreprises_MGP/part_autoEnrepreneur.csv',
+//       enablePolling: true
+//   },
 
-  credits: {
-    enabled:false
-  },
- tooltip: {
-  pointFormat: '<span style="color: grey">{series.name}</span>: <b>{point.y} %</b><br/>',
-  valueDecimals: 1,
-  color:'#f0836d',
-  shared: true,
-},
-  yAxis: {
-    gridLineColor: '#efefef',
-    gridLineDashStyle: 'dash',
-    labels: {
-      format: '{value} %',
-      // formatter: function() {
-      //   return Highcharts.numberFormat(this.value, 0, '.', ' ');
-      // },
-        style: {
-          color: '#CFCFCF'
-          }
-        },
-   title: {
-      enabled: false,
-    }
-   },
-   plotOptions: {
-    series: {
-      lineWidth: 1,
-      marker: {
-        // enabled: false,
-        Symbol:"circle",
-        radius: 3,
-        fillColor: '#FFFFFF',
-        lineWidth: 2,
-        lineColor: null // inherit from series,
-        
-      },
-    //   dataLabels: {
-    //      enabled: true,
-    //      align: 'left',
-    //      crop: false,
-    //      useHTML: false,
-    //      formatter: function() {
-    //          if (this.point.x == this.series.data.length - 1) {
-    //            return '<span style="color: '+ this.color + '">' + Highcharts.numberFormat(this.y,0) + '</span>';
-    //          } else {
-    //              return null;
-    //          }
-    //      },
-    //  }
-  }
-},
-
-  legend: {
-   enabled: true
-  },
-//   plotOptions: {
-//       series: {
-//         lineWidth: 1,
-//         marker: {
-//           enabled: false,
-//           radius: 3,
-//           fillColor: '#FFFFFF',
-//           lineWidth: 2,
-//           lineColor: null // inherit from series,
-//         },
-//         dataLabels: {
-//            enabled: true,
-//            align: 'left',
-//            crop: false,
-//            useHTML: false,
-//            formatter: function() {
-//                if (this.point.x == this.series.data.length - 1) {
-//                  return '<span style="color: '+ this.color + '">' + Highcharts.numberFormat(this.y,0) + '</span>';
-//                } else {
-//                    return null;
-//                }
-//            },
-//        }
-//     }
+//   credits: {
+//     enabled:false
+//   },
+//  tooltip: {
+//   pointFormat: '<span style="color: grey">{series.name}</span>: <b>{point.y} %</b><br/>',
+//   valueDecimals: 1,
+//   color:'#f0836d',
+//   shared: true,
 // },
-responsive: {
-    rules: [{
-        condition: {
-            maxWidth: 560
-        },
-        // Labels space on mobile
-        chartOptions: {
-            xAxis: {
-                labels: {
-                    step: 4,
-                }
-            },
-        }
-    }]
-},
-  exporting: {
-    filename: 'Demandeur-d-emploi__Observatoire-economie-parisienne__Apur',
-    chartOptions:{
-      title: {
-        text: "."
-      },
-      caption: {
-        text: "Source : Pôle emploi-Dares, STMT ; données CVS-CJO. Un demandeur d’emploi est une personne inscrite sur les listes de Pôle emploi. L’inscription sur les listes de Pôle emploi est soumise à certaines conditions, mais les demandeurs d’emploi peuvent être ou non indemnisés, certains peuvent occuper un emploi. https://www.apur.org/fr/geo-data/observatoire-economie-parisienne-donnees-conjoncturelles"
-      },
-        chart:{
-         events:{
-             load:function(){
-                // add logo, titles, and sources update in in the html page
-                 this.renderer.image('https://atelier-parisien-d-urbanisme.github.io/Observatoire-economie-paris/logo_apur.png', 0, 0, 50, 30).add();
+//   yAxis: {
+//     gridLineColor: '#efefef',
+//     gridLineDashStyle: 'dash',
+//     labels: {
+//       format: '{value} %',
+//       // formatter: function() {
+//       //   return Highcharts.numberFormat(this.value, 0, '.', ' ');
+//       // },
+//         style: {
+//           color: '#CFCFCF'
+//           }
+//         },
+//    title: {
+//       enabled: false,
+//     }
+//    },
+//    plotOptions: {
+//     series: {
+//       lineWidth: 1,
+//       marker: {
+//         // enabled: false,
+//         Symbol:"circle",
+//         radius: 3,
+//         fillColor: '#FFFFFF',
+//         lineWidth: 2,
+//         lineColor: null // inherit from series,
+        
+//       },
+//     //   dataLabels: {
+//     //      enabled: true,
+//     //      align: 'left',
+//     //      crop: false,
+//     //      useHTML: false,
+//     //      formatter: function() {
+//     //          if (this.point.x == this.series.data.length - 1) {
+//     //            return '<span style="color: '+ this.color + '">' + Highcharts.numberFormat(this.y,0) + '</span>';
+//     //          } else {
+//     //              return null;
+//     //          }
+//     //      },
+//     //  }
+//   }
+// },
 
-                 title = document.getElementById('title1').innerHTML;
-                 this.renderer.text(title, 70, 20)
-                  .css({
-                    color: 'black',
-                    fontFamily: 'Roboto',
-                    fontSize: 14,
-                    fontWeight: 'bold',
-                    width: 400
-                  }).add().toFront();
-             }
-         }
-     }
-   },
-          buttons: {
-              contextButton: {
-                  menuItems: [
-                      'printChart',
-                      'separator',
-                      'downloadPNG',
-                      'downloadJPEG',
-                      'downloadPDF',
-                      'downloadSVG'
-                  ]
-              }
-          }
-      }
+//   legend: {
+//    enabled: true
+//   },
+// //   plotOptions: {
+// //       series: {
+// //         lineWidth: 1,
+// //         marker: {
+// //           enabled: false,
+// //           radius: 3,
+// //           fillColor: '#FFFFFF',
+// //           lineWidth: 2,
+// //           lineColor: null // inherit from series,
+// //         },
+// //         dataLabels: {
+// //            enabled: true,
+// //            align: 'left',
+// //            crop: false,
+// //            useHTML: false,
+// //            formatter: function() {
+// //                if (this.point.x == this.series.data.length - 1) {
+// //                  return '<span style="color: '+ this.color + '">' + Highcharts.numberFormat(this.y,0) + '</span>';
+// //                } else {
+// //                    return null;
+// //                }
+// //            },
+// //        }
+// //     }
+// // },
+// responsive: {
+//     rules: [{
+//         condition: {
+//             maxWidth: 560
+//         },
+//         // Labels space on mobile
+//         chartOptions: {
+//             xAxis: {
+//                 labels: {
+//                     step: 4,
+//                 }
+//             },
+//         }
+//     }]
+// },
+//   exporting: {
+//     filename: 'Demandeur-d-emploi__Observatoire-economie-parisienne__Apur',
+//     chartOptions:{
+//       title: {
+//         text: "."
+//       },
+//       caption: {
+//         text: "Source : Pôle emploi-Dares, STMT ; données CVS-CJO. Un demandeur d’emploi est une personne inscrite sur les listes de Pôle emploi. L’inscription sur les listes de Pôle emploi est soumise à certaines conditions, mais les demandeurs d’emploi peuvent être ou non indemnisés, certains peuvent occuper un emploi. https://www.apur.org/fr/geo-data/observatoire-economie-parisienne-donnees-conjoncturelles"
+//       },
+//         chart:{
+//          events:{
+//              load:function(){
+//                 // add logo, titles, and sources update in in the html page
+//                  this.renderer.image('https://atelier-parisien-d-urbanisme.github.io/Observatoire-economie-paris/logo_apur.png', 0, 0, 50, 30).add();
 
-});
+//                  title = document.getElementById('title1').innerHTML;
+//                  this.renderer.text(title, 70, 20)
+//                   .css({
+//                     color: 'black',
+//                     fontFamily: 'Roboto',
+//                     fontSize: 14,
+//                     fontWeight: 'bold',
+//                     width: 400
+//                   }).add().toFront();
+//              }
+//          }
+//      }
+//    },
+//           buttons: {
+//               contextButton: {
+//                   menuItems: [
+//                       'printChart',
+//                       'separator',
+//                       'downloadPNG',
+//                       'downloadJPEG',
+//                       'downloadPDF',
+//                       'downloadSVG'
+//                   ]
+//               }
+//           }
+//       }
+
+// });
 
 
 
@@ -335,7 +335,7 @@ Highcharts.chart('graphiqueOld', {
         text: null
     },
     data: {
-        csvURL: 'https://raw.githubusercontent.com/Atelier-parisien-d-urbanisme/Observatoire-economie-paris/main/4_Creations_et_defaillances_d_entreprises/4_1_Creations_entreprises_MGP/creation_entreprise_PartEntrepreneurs.csv'
+        csvURL: 'https://raw.githubusercontent.com/Atelier-parisien-d-urbanisme/Observatoire-economie-paris/main/4_Creations_et_defaillances_d_entreprises/4_1_Creations_entreprises_MGP/creations_entreprises.csv'
     },
     credits: {
       enabled:false
@@ -372,7 +372,7 @@ Highcharts.chart('graphiqueOld', {
       return Highcharts.numberFormat(this.value, 0, '.', ' ');
     },
           style: {
-         
+            color: '#CECECE'
           }
       },
   opposite: true
@@ -387,7 +387,8 @@ plotOptions: {
         lineWidth: 2,
         lineColor: null // inherit from series,
       }
-  }
+  },
+ 
 },
 series: [
       {
@@ -397,6 +398,8 @@ series: [
         tooltip: {pointFormat: '<span style="color: grey">{series.name}</span>: <b>{point.y} %</b><br/>',
         valueDecimals: 0,
         // lineWidth:3,
+
+        
       }
     },
   {
@@ -421,6 +424,19 @@ series: [
     valueDecimals: 0,
 
   },
+  dataLabels: {
+    enabled: true,
+    align: 'left',
+    crop: false,
+    useHTML: false,
+    formatter: function() {
+        if (this.point.x == this.series.data.length - 1) {
+          return '<span style="color: '+ this.color + '">' + Highcharts.numberFormat(this.y,0) + '</span>';
+        } else {
+            return null;
+        }
+    },
+}
   
 },
 {
@@ -436,6 +452,19 @@ series: [
   tooltip: {pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b><br/>',
   valueDecimals: 0,
 },
+dataLabels: {
+  enabled: true,
+  align: 'left',
+  crop: false,
+  useHTML: false,
+  formatter: function() {
+      if (this.point.x == this.series.data.length - 1) {
+        return '<span style="color: '+ this.color + '">' + Highcharts.numberFormat(this.y,0) + '</span>';
+      } else {
+          return null;
+      }
+  },
+}
 
 }
 
