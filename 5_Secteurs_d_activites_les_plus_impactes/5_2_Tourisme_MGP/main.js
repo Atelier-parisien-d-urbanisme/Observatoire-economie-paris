7637f60e58b9569b1a6cb14dfc6517cd62de52df
anaismoreaugeo <anaismoreau.geo@gmail.com>
2022-10-03 20:04:43 +0200

    init

--- 5_Secteurs_d_activites_les_plus_impactes/5_2_Tourisme_MGP/main.js
+++ 5_Secteurs_d_activites_les_plus_impactes/5_2_Tourisme_MGP/main.js
@@ -24,118 +24,153 @@ Highcharts.setOptions({
   }
 });
 
-Highcharts.chart('graphique1', {
-  chart: {
-  },
-    title: {
-        text: null
-    },
-    data: {
-        csvURL: 'https://raw.githubusercontent.com/Atelier-parisien-d-urbanisme/Observatoire-economie-paris/main/5_Secteurs_d_activites_les_plus_impactes/5_2_Tourisme/tourisme.csv',
-        startColumn:0,
-        endColumn:4,
-    },
-    plotOptions: {
-        series: {
-          lineWidth: 1,
-            marker: {
-              symbol: 'circle',
-              enabledThreshold: 2,
-              radius: 3,
-              fillColor: '#FFFFFF',
-              lineWidth: 2,
-              lineColor: null // inherit from series,
-            }
-        }
-    },
 
-    yAxis: [{
-      gridLineColor: '#efefef',
-      gridLineDashStyle: 'dash',
-     title: {
-        enabled:false,
-    },
-    labels: {
-    format: '{value}%',
-    // align: 'left',
-    //       x: 0,
-    //       y: -2,
-          style: {
-              color: '#CFCFCF'
-          }
-  }
-}]
-,
-series: [{
-    type:'column',
-    yAxis: 0,
-    data: {
-    },
-    tooltip: {pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y} %</b><br/>',
-    valueDecimals: 0
-  },
-  dataLabels: {
-               enabled: true,
-               format: '{y}%',
-               style: {
-                fontSize: '10px',
-                fontFamily: 'Roboto',
-                // color:'#38cae9',
-            }
-           },
-},{
-  type:'spline',
-    yAxis: 0,
-    data: {
-    },
-    tooltip: {pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y} %</b><br/>',
-    valueDecimals: 0
-   }
-}],
-     exporting: {
-       filename: 'Occupation-Hotel__Observatoire-economie-parisienne__Apur',
-       chartOptions:{
-         // add logo, titles, and sources updated in in the html page
-         title: {
-           text: "."
-         },
-         caption: {
-           text: "Source : OTCP. https://www.apur.org/fr/geo-data/observatoire-economie-parisienne-donnees-conjoncturelles"
-         },
-           chart:{
-            events:{
-                load:function(){
-                    this.renderer.image('https://atelier-parisien-d-urbanisme.github.io/Observatoire-economie-paris/logo_apur.png', 0, 0, 50, 30).add();
+let urlParis = 'https://raw.githubusercontent.com/Atelier-parisien-d-urbanisme/Observatoire-economie-paris/main/5_Secteurs_d_activites_les_plus_impactes/5_2_Tourisme_MGP/tourisme.csv'
 
-                    title = document.getElementById('title1').innerHTML;
-                    this.renderer.text(title, 70, 20)
-                     .css({
-                       color: 'black',
-                       fontFamily: 'Roboto',
-                       fontSize: 14,
-                       fontWeight: 'bold',
-                       width: 400
-                     }).add().toFront();
-                }
-            }
-        }
+let urlMGP ='https://raw.githubusercontent.com/Atelier-parisien-d-urbanisme/Observatoire-economie-paris/main/5_Secteurs_d_activites_les_plus_impactes/5_2_Tourisme_MGP/tourisme_MGP.csv'
+
+let url = urlParis;
+
+
+
+
+function getGraphique() {
+  graphChart = new Highcharts.chart('graphique1', {
+    chart: {
+    },
+      title: {
+          text: null
       },
-         buttons: {
-             contextButton: {
-                 menuItems: [
-                     'printChart',
-                     'separator',
-                     'downloadPNG',
-                     'downloadJPEG',
-                     'downloadPDF',
-                     'downloadSVG'
-                 ]
-             }
-         }
-     },
-     credits: {
-       enabled:false
+      data: {
+          csvURL: url,
+          startColumn:0,
+          endColumn:4,
+      },
+      plotOptions: {
+          series: {
+            lineWidth: 1,
+              marker: {
+                symbol: 'circle',
+                enabledThreshold: 2,
+                radius: 3,
+                fillColor: '#FFFFFF',
+                lineWidth: 2,
+                lineColor: null // inherit from series,
+              }
+          }
+      },
+  
+      yAxis: [{
+        gridLineColor: '#efefef',
+        gridLineDashStyle: 'dash',
+       title: {
+          enabled:false,
+      },
+      labels: {
+      format: '{value}%',
+      // align: 'left',
+      //       x: 0,
+      //       y: -2,
+            style: {
+                color: '#CFCFCF'
+            }
+    }
+  }]
+  ,
+  series: [{
+      type:'column',
+      yAxis: 0,
+      data: {
+      },
+      tooltip: {pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y} %</b><br/>',
+      valueDecimals: 0
+    },
+    dataLabels: {
+                 enabled: true,
+                 format: '{y}%',
+                 style: {
+                  fontSize: '10px',
+                  fontFamily: 'Roboto',
+                  // color:'#38cae9',
+              }
+             },
+  },{
+    type:'spline',
+      yAxis: 0,
+      data: {
+      },
+      tooltip: {pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y} %</b><br/>',
+      valueDecimals: 0
+     }
+  }],
+       exporting: {
+         filename: 'Occupation-Hotel__Observatoire-economie-parisienne__Apur',
+         chartOptions:{
+           // add logo, titles, and sources updated in in the html page
+           title: {
+             text: "."
+           },
+           caption: {
+             text: "Source : OTCP. https://www.apur.org/fr/geo-data/observatoire-economie-parisienne-donnees-conjoncturelles"
+           },
+             chart:{
+              events:{
+                  load:function(){
+                      this.renderer.image('https://atelier-parisien-d-urbanisme.github.io/Observatoire-economie-paris/logo_apur.png', 0, 0, 50, 30).add();
+  
+                      title = document.getElementById('title1').innerHTML;
+                      this.renderer.text(title, 70, 20)
+                       .css({
+                         color: 'black',
+                         fontFamily: 'Roboto',
+                         fontSize: 14,
+                         fontWeight: 'bold',
+                         width: 400
+                       }).add().toFront();
+                  }
+              }
+          }
+        },
+           buttons: {
+               contextButton: {
+                   menuItems: [
+                       'printChart',
+                       'separator',
+                       'downloadPNG',
+                       'downloadJPEG',
+                       'downloadPDF',
+                       'downloadSVG'
+                   ]
+               }
+           }
        },
+       credits: {
+         enabled:false
+         },
+  });
+  
+}
+getGraphique();
+
+mgpBtn = document.getElementById('btn-mgp');
+
+mgpBtn.addEventListener('click', function () {
+  url = urlMGP;
+  graphChart.destroy();
+  getGraphique();
+  this.classList.add('active');
+  parisBtn.classList.remove('active');
+});
+
+
+parisBtn = document.getElementById('btn-paris');
+
+parisBtn.addEventListener('click', function () {
+  url = urlParis;
+  graphChart.destroy();
+  getGraphique();
+  this.classList.add('active');
+  mgpBtn.classList.remove('active');
 });
 
 
@@ -151,19 +186,19 @@ Highcharts.chart('graphique2', {
     },
 
     data: {
-        csvURL: 'https://raw.githubusercontent.com/Atelier-parisien-d-urbanisme/Observatoire-economie-paris/main/5_Secteurs_d_activites_les_plus_impactes/5_2_Tourisme/tourisme2.csv',
+        csvURL: 'https://raw.githubusercontent.com/Atelier-parisien-d-urbanisme/Observatoire-economie-paris/main/5_Secteurs_d_activites_les_plus_impactes/5_2_Tourisme_MGP/tourisme2.csv',
         enablePolling: true
     },
 
     credits: {
       enabled:false
     },
-
     plotOptions: {
         series: {
-          color:'#CE8C34',
           lineWidth: 1,
             marker: {
+              symlbol : 'circle',
+              enabled:false,
               enabledThreshold: 2,
               radius: 3,
               fillColor: '#FFFFFF',
@@ -200,6 +235,14 @@ Highcharts.chart('graphique2', {
             }
         }]
     },
+    series : [
+      {
+        color: '#E69818',
+      },
+      {
+        color:'#7E1500',
+      }
+    ],
     xAxis: {
     labels: {
     step: 1,
@@ -212,8 +255,8 @@ Highcharts.chart('graphique2', {
     yAxis: {
       gridLineColor: '#efefef',
       gridLineDashStyle: 'dash',
-      min:100000,
-      max:180000,
+      // min:100000,
+      // max:180000,
       labels: {
         format: '{value}',
         formatter: function() {
@@ -227,12 +270,10 @@ Highcharts.chart('graphique2', {
         enabled: false,
       }
      },
-    legend: {
-     enabled: false
-    },
     tooltip: {
         pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b><br/>',
-        valueDecimals: 0
+        valueDecimals: 0,
+        shared :true,
     },
     exporting: {
       filename: 'Hotel-emploi-salarie__Observatoire-economie-parisienne__Apur',
