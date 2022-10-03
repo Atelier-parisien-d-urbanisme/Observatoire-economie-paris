f828ba9b7aa1760aaf66e2e87d45bacd76daccb0
anaismoreaugeo <anaismoreau.geo@gmail.com>
2022-10-03 19:39:42 +0200

    init

--- 4_Creations_et_defaillances_d_entreprises/4_3_Ouvertures_procedures_collectives_MGP/main.js
+++ 4_Creations_et_defaillances_d_entreprises/4_3_Ouvertures_procedures_collectives_MGP/main.js
@@ -26,117 +26,117 @@ Highcharts.setOptions({
 
 let urlParis = 'https://raw.githubusercontent.com/Atelier-parisien-d-urbanisme/Observatoire-economie-paris/main/4_Creations_et_defaillances_d_entreprises/4_3_Ouvertures_procedures_collectives/ouverture_procedures_collectives.csv'
 
-let urlMGP = 'https://raw.githubusercontent.com/Atelier-parisien-d-urbanisme/Observatoire-economie-paris/main/4_Creations_et_defaillances_d_entreprises/4_3_Ouvertures_procedures_collectives/ouvertures_procedures_collectives_MGP.csv'
+let urlMGP = 'https://raw.githubusercontent.com/Atelier-parisien-d-urbanisme/Observatoire-economie-paris/main/4_Creations_et_defaillances_d_entreprises/4_3_Ouvertures_procedures_collectives/ouverture_procedures_collectives_MGP.csv'
 
 let url = urlParis;
 
 
-Highcharts.chart('graphique', {
-  chart: {
-    type:'line',
-  },
-    title: {
-        text: null
-    },
-    data: {
-        csvURL: url,
-        startColumn:0,
-        endColumn:4,
-    },
-    plotOptions: {
-        series: {
-          lineWidth: 1,
-            marker: {
-              enabledThreshold: 2,
-              radius: 3,
-              sumbols:'circle',
-              fillColor: '#FFFFFF',
-              lineWidth: 2,
-              lineColor: null // inherit from series,
-            }
-        }
-    },
+// Highcharts.chart('graphique', {
+//   chart: {
+//     type:'line',
+//   },
+//     title: {
+//         text: null
+//     },
+//     data: {
+//         csvURL: url,
+//         startColumn:0,
+//         endColumn:4,
+//     },
+//     plotOptions: {
+//         series: {
+//           lineWidth: 1,
+//             marker: {
+//               enabledThreshold: 2,
+//               radius: 3,
+//               sumbols:'circle',
+//               fillColor: '#FFFFFF',
+//               lineWidth: 2,
+//               lineColor: null // inherit from series,
+//             }
+//         }
+//     },
 
-    yAxis: [{
-      gridLineColor: '#efefef',
-      gridLineDashStyle: 'dash',
-      labels: {
-        format:'{value}',
-        // align: 'left',
-        //       x: 0,
-        //       y: -2,
-              style: {
-                  color: '#CFCFCF'
-              }
-          },
-     title: {
-        enabled: false,
-      }
-      }]
-,
-series: [{
-    type:'column',
-    yAxis: 0,
-    data: {
-    },
-    tooltip: {pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b><br/>',
-    valueDecimals: 0
-   }
-},{
-  type:'spline',
-    yAxis: 0,
-    data: {
-    },
-    tooltip: {pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b><br/>',
-    valueDecimals: 0
-   }
-}],
-     exporting: {
-       filename: 'Ouverture_procedures_collectives__Observatoire-economie-parisienne__Apur',
-       chartOptions:{
-         // add logo, titles, and sources updated in in the html page
-         title: {
-           text: "."
-         },
-         caption: {
-           text: "Source : Greffe du tribunal de commerce. La procédure collective est une procédure de redressement ou de liquidation judiciaire organisant le règlement du paiement des créances d'une entreprise en cessation de paiement. https://www.apur.org/fr/geo-data/observatoire-economie-parisienne-donnees-conjoncturelles"
-         },
-           chart:{
-            events:{
-                load:function(){
-                    this.renderer.image('https://atelier-parisien-d-urbanisme.github.io/Observatoire-economie-paris/logo_apur.png', 0, 0, 50, 30).add();
+//     yAxis: [{
+//       gridLineColor: '#efefef',
+//       gridLineDashStyle: 'dash',
+//       labels: {
+//         format:'{value}',
+//         // align: 'left',
+//         //       x: 0,
+//         //       y: -2,
+//               style: {
+//                   color: '#CFCFCF'
+//               }
+//           },
+//      title: {
+//         enabled: false,
+//       }
+//       }]
+// ,
+// series: [{
+//     type:'column',
+//     yAxis: 0,
+//     data: {
+//     },
+//     tooltip: {pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b><br/>',
+//     valueDecimals: 0
+//    }
+// },{
+//   type:'spline',
+//     yAxis: 0,
+//     data: {
+//     },
+//     tooltip: {pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b><br/>',
+//     valueDecimals: 0
+//    }
+// }],
+//      exporting: {
+//        filename: 'Ouverture_procedures_collectives__Observatoire-economie-parisienne__Apur',
+//        chartOptions:{
+//          // add logo, titles, and sources updated in in the html page
+//          title: {
+//            text: "."
+//          },
+//          caption: {
+//            text: "Source : Greffe du tribunal de commerce. La procédure collective est une procédure de redressement ou de liquidation judiciaire organisant le règlement du paiement des créances d'une entreprise en cessation de paiement. https://www.apur.org/fr/geo-data/observatoire-economie-parisienne-donnees-conjoncturelles"
+//          },
+//            chart:{
+//             events:{
+//                 load:function(){
+//                     this.renderer.image('https://atelier-parisien-d-urbanisme.github.io/Observatoire-economie-paris/logo_apur.png', 0, 0, 50, 30).add();
 
-                    title = document.getElementById('title1').innerHTML;
-                    this.renderer.text(title, 70, 20)
-                     .css({
-                       color: 'black',
-                       fontFamily: 'Roboto',
-                       fontSize: 14,
-                       fontWeight: 'bold',
-                       width: 400
-                     }).add().toFront();
-                }
-            }
-        }
-      },
+//                     title = document.getElementById('title1').innerHTML;
+//                     this.renderer.text(title, 70, 20)
+//                      .css({
+//                        color: 'black',
+//                        fontFamily: 'Roboto',
+//                        fontSize: 14,
+//                        fontWeight: 'bold',
+//                        width: 400
+//                      }).add().toFront();
+//                 }
+//             }
+//         }
+//       },
 
-         buttons: {
-             contextButton: {
-                 menuItems: [
-                     'printChart',
-                     'separator',
-                     'downloadPNG',
-                     'downloadJPEG',
-                     'downloadPDF',
-                     'downloadSVG'
-                 ]
-             }
-         }
-     },
-     credits: {
-       enabled:false
-       },
-});
+//          buttons: {
+//              contextButton: {
+//                  menuItems: [
+//                      'printChart',
+//                      'separator',
+//                      'downloadPNG',
+//                      'downloadJPEG',
+//                      'downloadPDF',
+//                      'downloadSVG'
+//                  ]
+//              }
+//          }
+//      },
+//      credits: {
+//        enabled:false
+//        },
+// });
 
 
 
