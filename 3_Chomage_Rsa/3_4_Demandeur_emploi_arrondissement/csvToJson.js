a064a52e42e3cf02f11782c894366ea67dac0a4e
anaismoreaugeo <anaismoreau.geo@gmail.com>
2023-03-31 15:53:40 +0200

    init

--- 3_Chomage_Rsa/3_4_Demandeur_emploi_arrondissement/csvToJson.js
+++ 3_Chomage_Rsa/3_4_Demandeur_emploi_arrondissement/csvToJson.js
@@ -1,17 +1,12 @@
-var jsonData = [];
+const csvUrl = 'https://atelier-parisien-d-urbanisme.github.io/Observatoire-economie-paris/3_Chomage_Rsa/3_4_Demandeur_emploi_arrondissement/map1.csv';
 
-fetch('https://atelier-parisien-d-urbanisme.github.io/Observatoire-economie-paris/3_Chomage_Rsa/3_4_Demandeur_emploi_arrondissement/map1.csv')
+fetch(csvUrl)
   .then(response => response.text())
-  .then(data => {
-    var lines = data.split('\n');
-    var headers = lines[0].split(',');
-    for (var i = 1; i < lines.length; i++) {
-      var values = lines[i].split(',');
-      var obj = {};
-      for (var j = 0; j < headers.length; j++) {
-        obj[headers[j]] = values[j];
-      }
-      jsonData.push(obj);
-    }
-    console.log(jsonData);
+  .then(csv => {
+    const lines = csv.split('\n');
+    const headers = lines[0].split(';');
+    const data = lines[1].split(';');
+
+    const arrondissements = headers.map((code, index) => [code, parseFloat(data[index].replace(',', '.'))]);
+    console.log(arrondissements);
   });
