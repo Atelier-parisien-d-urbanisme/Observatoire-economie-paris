2c547a03d0b6658f8348c31bc15a3b37b1008e00
Goyo <79099818+gregoirecatineau@users.noreply.github.com>
2025-01-08 10:51:23 +0100

    Update main.js
    Alignement du label à droite

--- 3_Chomage_Rsa/3_2_Demandeur_emploi/main.js
+++ 3_Chomage_Rsa/3_2_Demandeur_emploi/main.js
@@ -96,7 +96,7 @@ Highcharts.chart('graphique', {
           },
           dataLabels: {
              enabled: true,
-             align: 'left',
+             align: 'right',
              crop: false,
              useHTML: false,
              formatter: function() {
