e4ecc57773c0034711e42c1cafe40926654b775e
Goyo <79099818+gregoirecatineau@users.noreply.github.com>
2025-01-08 10:39:06 +0100

    Update main.js
    Alignement du label à droite

--- 2_Evolution_de_l_emploi/2_2_Emploi_interimaire/main.js
+++ 2_Evolution_de_l_emploi/2_2_Emploi_interimaire/main.js
@@ -71,7 +71,7 @@ Highcharts.chart('graphique', {
           },
           dataLabels: {
              enabled: true,
-             align: 'left',
+             align: 'right',
              crop: false,
              useHTML: false,
              formatter: function() {
